d5190b87b709ff611b9bed1c48a5b14bec2df4e9
Rodrigo237 <36214941+Rodrigo237@users.noreply.github.com>
2025-08-07 22:45:41 -0700

    Exercise Done

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 $(function() {
   // your code here
+  $(".accordion .accordion-header").on("click", function () {
+  $(".accordion .accordion-content").not($(this).next()).slideUp();
+
+  $(this).next(".accordion-content").slideToggle("fast", function () {
+    console.log("Section toggled");
+  });
+  });
+
+  $('.todos').on('click', function() {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      type: "GET",
+      success: function(response) {
+        const todos = response.todos
+        buildTodoList(todos)
+      },
+      error: function(err) {
+        console.error(err)
+      }
+    })
+  })
 })
+
+function buildTodoList(arr) {
+    const list = $('.todos ul')
+    let liHtml = '' // store list
+    arr.forEach(el => {
+      liHtml += `<li>${el.todo}</li>` // concatenate li into variable
+    })
+    list.html(liHtml)
+  }
